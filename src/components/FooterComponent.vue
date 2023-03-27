8fd0a648bf6086a238d45e3fc36b50096244e437
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-03-27 10:41:46 +0200

    remove footer text

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,7 +2,6 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col">
-				<h3 class="text-center">Footer</h3>
 				<p class="text-center">Created by Johan Hellberg 2023. Website, <a href="https://jhellberg.com"
 						target="_blank">jhellberg.com</a>
 				</p>
