116419877429fa6071dc30197fbf918a569323d3
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-03-27 11:07:54 +0200

    change filter function to work

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -1,37 +1,37 @@
-import { defineStore } from "pinia";
-import axios from "axios";
-import Product from "../types/Product";
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import Product from '../types/Product'
 
-export const useProductStore = defineStore("ProductStore", {
+export const useProductStore = defineStore('ProductStore', {
   state: () => {
     return {
       products: [] as Product[],
       filteredProducts: [] as Product[],
-    };
+    }
   },
   actions: {
     async fill() {
       this.products = await axios
-        .get("https://fakestoreapi.com/products")
+        .get('https://fakestoreapi.com/products')
         .then((response) => (this.products = response.data))
-        .catch((error) => console.log(error));
+        .catch((error) => console.log(error))
     },
     search(searchQuery: string) {
-      if (searchQuery === "") return (this.filteredProducts = this.products);
+      if (searchQuery === '') return (this.filteredProducts = this.products)
       this.filteredProducts = this.products.filter((product) => {
-        return product.title.toLowerCase().includes(searchQuery.toLowerCase());
-      });
+        return product.title.toLowerCase().includes(searchQuery.toLowerCase())
+      })
     },
   },
   getters: {
-    filter() {
-      this.filteredProducts = this.products.filter((product) => {
+    filter({ products }) {
+      return products.filter((product: Product) => {
         return (
           product.category === "men's clothing" ||
           product.category === "women's clothing" ||
-          product.category === "jewelery"
-        );
-      });
+          product.category === 'jewelery'
+        )
+      })
     },
   },
-});
+})
