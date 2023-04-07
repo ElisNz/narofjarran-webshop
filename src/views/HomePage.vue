6f3ab164c8393baf9d51a8a53f23a8cab8e8b86b
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-07 15:51:01 +0200

    fix eslint parser

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,13 +6,19 @@
   <div class="container">
     <div class="row">
       <div class="col-4">
-        <button @click="handleClickProducts('id')" class="btn btn-secondary">Order by id</button>
+        <button @click="handleClickProducts('id')" class="btn btn-secondary">
+          Order by id
+        </button>
       </div>
       <div class="col-4">
-        <button @click="handleClickProducts('title')" class="btn btn-secondary">Order by title</button>
+        <button @click="handleClickProducts('title')" class="btn btn-secondary">
+          Order by title
+        </button>
       </div>
       <div class="col-4">
-        <button @click="handleClickProducts('price')" class="btn btn-secondary">Order by price</button>
+        <button @click="handleClickProducts('price')" class="btn btn-secondary">
+          Order by price
+        </button>
       </div>
       <ProductList :order="orderProduct" />
     </div>
@@ -20,23 +26,25 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import ProductList from '../components/ProductList.vue';
-import NavBar from '../components/NavBar.vue';
-import SearchBar from '../components/SearchBar.vue'
-import OrderTermProducts from '../types/OrderTermProducts';
-import HeroProduct from '../components/HeroProduct.vue';
-import HeroInspiration from '../components/HeroInspiration.vue';
-import HeroMessage from '../components/HeroMessage.vue';
-import FooterComponent from '../components/FooterComponent.vue';
-import { useProductStore } from '../stores/ProductStore';
+import { defineComponent, ref } from 'vue'
+import ProductList from '../components/ProductList.vue'
+import OrderTermProducts from '../types/OrderTermProducts'
+import HeroProduct from '../components/HeroProduct.vue'
+import HeroInspiration from '../components/HeroInspiration.vue'
+import HeroMessage from '../components/HeroMessage.vue'
+import { useProductStore } from '../stores/ProductStore'
 
 export default defineComponent({
   name: 'App',
-  components: { HeroInspiration, HeroMessage, HeroProduct, NavBar, ProductList, FooterComponent, SearchBar },
+  components: {
+    HeroInspiration,
+    HeroMessage,
+    HeroProduct,
+    ProductList,
+  },
   setup() {
-    const productStore = useProductStore();
-    productStore.fill();
+    const productStore = useProductStore()
+    productStore.fill()
 
     const orderProduct = ref<OrderTermProducts>('id')
     const handleClickProducts = (term: OrderTermProducts) => {
