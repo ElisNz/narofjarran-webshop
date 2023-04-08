b564dea9f64f760adbd5dd2943bd45e53f8b57fd
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-08 22:45:21 +0200

    fix routerlink class and format on save

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,43 @@
 <template>
-	<header>
-		<nav class="py-2 bg-light border-bottom">
-			<div class="container d-flex flex-wrap">
-				<ul class="nav me-auto">
-					<router-link to="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
-						<img src="../assets/logo.svg" alt="site logo" class="navbar-brand nav-logo" width="42" height="42">
-					</router-link>
-					<li class="nav-item"><router-link to="/" class="nav-link link-dark px-2 active" aria-current="page">Home</router-link></li>
-					<li class="nav-item"><router-link to="/about" class="nav-link link-dark px-2">About</router-link></li>
-				</ul>
-				<CartWidget />
-			</div>
-		</nav>
-	</header>
+  <header>
+    <nav class="py-2 bg-light border-bottom">
+      <div class="container d-flex flex-wrap">
+        <ul class="nav me-auto">
+          <router-link to="/" class="nav-item">
+            <img
+              src="../assets/logo.svg"
+              alt="site logo"
+              class="navbar-brand nav-logo"
+              width="42"
+              height="42"
+            />
+          </router-link>
+          <li class="nav-item">
+            <router-link
+              to="/"
+              class="nav-link link-dark px-2 active"
+              aria-current="page"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link link-dark px-2"
+              >About</router-link
+            >
+          </li>
+        </ul>
+        <CartWidget />
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script setup lang="ts">
-import CartWidget from './CartWidget.vue';
-
+import CartWidget from './CartWidget.vue'
 </script>
 
 <style scoped>
-body { padding-top: 70px; }
+body {
+  padding-top: 70px;
+}
 </style>
