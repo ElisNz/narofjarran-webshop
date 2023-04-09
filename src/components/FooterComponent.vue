5d772e982b156743ac546fed2bad9cafa678d27c
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-09 11:12:33 +0200

    format on save

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,11 +1,12 @@
 <template>
-	<div class="container-fluid mt-5 p-3 bg-light border-top">
-		<div class="row">
-			<div class="col">
-				<p class="text-center">Created by Johan Hellberg 2023. Website, <a href="https://jhellberg.com"
-						target="_blank">jhellberg.com</a>
-				</p>
-			</div>
-		</div>
-	</div>
+  <div class="container-fluid mt-5 p-3 bg-light border-top">
+    <div class="row">
+      <div class="col">
+        <p class="text-center">
+          Created by Johan Hellberg 2023. Website,
+          <a href="https://jhellberg.com" target="_blank">jhellberg.com</a>
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
