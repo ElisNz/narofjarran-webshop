c960289dbb38628d4d6d8f1c19be54a5266fb120
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-09 11:11:46 +0200

    center header text

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,12 +1,16 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<p class="text-center">home / about {{ $route.params.id }}</p>
-				<h3>About This shop</h3>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa itaque tenetur dolore error nihil quisquam at
-					nobis ducimus corporis totam tempore, labore aperiam deleniti libero dicta maxime vero nisi? Aut, placeat.</p>
-			</div>
-		</div>
-	</div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <p class="text-center">home / about {{ $route.params.id }}</p>
+        <h3 class="text-center mb-3">About This shop</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa itaque
+          tenetur dolore error nihil quisquam at nobis ducimus corporis totam
+          tempore, labore aperiam deleniti libero dicta maxime vero nisi? Aut,
+          placeat.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,10 +1,17 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<h3 class="text-center">404 - Page Not Found</h3>
-				<p></p>
-			</div>
-		</div>
-	</div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <p class="text-center">home / 404</p>
+        <h3 class="text-center mb-3">Page Not Found - 404</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa itaque
+          tenetur dolore error nihil quisquam at nobis ducimus corporis totam
+          tempore, labore aperiam deleniti libero dicta maxime vero nisi? Aut,
+          placeat.
+        </p>
+        <p></p>
+      </div>
+    </div>
+  </div>
 </template>
