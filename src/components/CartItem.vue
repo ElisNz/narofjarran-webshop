b6d8a64f7a9e2acb1db8519b561b2c70b27827b3
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-09 10:28:15 +0200

    fix cols to keep content inside modal on mobile

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,27 +1,28 @@
 <script setup lang="ts">
-
 defineProps({
-	product: { type: Object, required: true },
-	count: { type: Number, required: true },
-});
+  product: { type: Object, required: true },
+  count: { type: Number, required: true },
+})
 
-defineEmits(["updateCount", "clear"]);
+defineEmits(['updateCount', 'clear'])
 </script>
 <template>
-	<div class="container mb-2">
-		<div class="row">
-			<div class="col-5">
-				<p>{{ product.title }}</p>
-			</div>
-			<div class="col-2">
-				<p>${{ count * product.price }}</p>
-			</div>
-			<div class="col-3">
-				<p>Quanitity: {{ count }}</p>
-			</div>
-			<div class="col-2">
-				<button @click="$emit('clear')" class="btn btn-danger p-2">Remove</button>
-			</div>
-		</div>
-</div>
+  <div class="container mb-2">
+    <div class="row">
+      <div class="col-4">
+        <p>{{ product.title }}</p>
+      </div>
+      <div class="col-3">
+        <p>${{ count * product.price }}</p>
+      </div>
+      <div class="col-2">
+        <p>Qty: {{ count }}</p>
+      </div>
+      <div class="col-2">
+        <button @click="$emit('clear')" class="btn btn-danger p-2">
+          Remove
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
