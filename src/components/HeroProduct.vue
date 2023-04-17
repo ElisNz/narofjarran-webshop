0b778714c3adc35ce54c890ec1272839de4db9d4
Johan Hellberg <hellberg.johan@hotmail.com>
2023-04-17 21:38:27 +0200

    remove unused styling

--- src/components/HeroProduct.vue
+++ src/components/HeroProduct.vue
@@ -3,7 +3,6 @@
     <div class="row">
       <div class="col">
         <div class="text-center">
-          <!-- <img class="d-block mx-auto mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
           <h3 class="display-5 fw-bold">{{ title }}</h3>
           <div class="col-lg-8 mx-auto">
             <img
@@ -38,22 +37,4 @@ export default defineComponent({
 .container {
   background-image: 'src/assets/dami-adebayo-k6aQzmIbR1s-unsplash.jpg';
 }
-
-/* div {
-		color: black;
-		display: flex;
-		align-items: center;
-		flex-direction: column;
-		justify-content: space-around;
-		height: 40vh;
-		width: 100vw;
-		background-color: #D8A7B1;
-	}
-	h3 {
-		text-align: center;
-	}
-	img {
-		width: 300px;
-		height: 150px;
-	} */
 </style>
